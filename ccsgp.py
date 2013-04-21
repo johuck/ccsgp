d56ce0f68319c6be5ac3a0dd7668378d5f9398a8
Patrick Huck <phuck@lbl.gov>
2013-04-20 18:07:00 -0700

    add function plotting

--- ccsgp.py
+++ ccsgp.py
@@ -7,9 +7,12 @@ import Gnuplot, Gnuplot.funcutils
 os.environ['GNUPLOT_PS_DIR'] = os.getcwd()
 
 class MyPlot:
-  def __init__(self, data):
+  def __init__(self, data, fdata=None):
     self.pts = Gnuplot.Data(data, inline=1, with_="points pt 18 lw 4 lc 1 ps 2")
     self.err = Gnuplot.Data(data, inline=1, with_="errorbars lc 0 lw 4")
+    self.func = None
+    if fdata is not None:
+      self.func = Gnuplot.Data(fdata, inline=1, with_="lines lc 0 lw 4 lt 1")
     self.gp = Gnuplot.Gnuplot(debug=1)
   def setEPS(self, n):
     self.epsname = n
@@ -18,6 +21,9 @@ class MyPlot:
   def setY(self, yt):
     self.gp.ylabel(yt)
   def plot(self):
-    self.gp.plot(self.err, self.pts)
+    if self.func is not None:
+      self.gp.plot(self.err, self.pts, self.func)
+    else:
+      self.gp.plot(self.err, self.pts)
     self.gp.hardcopy(self.epsname, enhanced=1, color=1,
                      mode='landscape', fontsize=22)
