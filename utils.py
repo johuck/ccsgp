34c700f2f9f6f1e9bb5162ab93e87246f4357cb8
Patrick Huck <phuck@lbl.gov>
2014-01-12 22:27:16 -0800

    add getOpts to utils

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 import itertools
+from config import default_colors
+
+def getOpts(i):
+  """convience function for easy access to gnuplot property string"""
+  return 'lt 1 lw 4 ps 2 lc %s pt 18' % default_colors[i]
 
 def zip_flat(a, b):
   """zip two lists and flattens the result
