02195e6529f24130233408bcfc72f89ff7cfa07c
Patrick Huck <phuck@lbl.gov>
2014-01-12 17:36:16 -0800

    update default_key, add default_colors

--- config.py
+++ config.py
@@ -9,10 +9,11 @@
 :var basic_setup: bars, grid, terminal and default_key
 :var default_margins: default margins to define plot area
 :var xPanProps: xscale, xsize, xoffset for panel plots
+:var default_colors: provides a reasonable color selection (up to 31)
 """
 
 default_key = [
-  'spacing 1.4', 'samplen 2.2', 'reverse Left',
+  'spacing 1.2', 'samplen 1.5', 'reverse Left',
   'box lw 2', 'height 0.5', 'font ",22"'
 ]
 
@@ -25,3 +26,15 @@ basic_setup = [
 default_margins = { 'l': 5, 'b': 1, 'r': 0.1, 't': 0.1 }
 
 xPanProps = [ 1.9, 0.23, 0.15 ]
+
+default_colors = [
+  '0', '1', '2', '3', '4', '5', '6', '9',
+  'rgb "#ff8c00"', 'rgb "#228b22"', 'rgb "#b22222"',
+  'rgb "#9370db"', 'rgb "#bdb76b"', 'rgb "#00bfff"',
+  'rgb "#fa8072"', 'rgb "#ee82ee"', 'rgb "#7fffd4"',
+  'rgb "#0000cd"', 'rgb "#ffdab9"', 'rgb "#eee9e9"',
+  'rgb "#eecbad"', 'rgb "#a0522d"', 'rgb "#2e8b57"',
+  'rgb "#3cb371"', 'rgb "#20b2aa"', 'rgb "#98fb98"',
+  'rgb "#db7093"', 'rgb "#b03060"', 'rgb "#c71585"',
+  'rgb "#bc8f8f"', 'rgb "#cd5c5c"'
+]
