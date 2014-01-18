aad9e567e32b0069c80d024cbf2dde3499998a67
Patrick Huck <phuck@lbl.gov>
2014-01-18 01:45:04 -0800

    utils: extend zip_flat to three

--- utils.py
+++ utils.py
@@ -5,8 +5,8 @@ def getOpts(i):
   """convience function for easy access to gnuplot property string"""
   return 'lt 1 lw 4 ps 2 lc %s pt 18' % default_colors[i]
 
-def zip_flat(a, b):
-  """zips two lists and flattens the result"""
+def zip_flat(a, b, c):
+  """zips three lists and flattens the result"""
   return list(
-    itertools.chain.from_iterable(zip(a, b))
+    itertools.chain.from_iterable(zip(a, b, c))
   )
