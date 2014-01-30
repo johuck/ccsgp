aa9f23780ac6dbd5f9405e23c9fc5d8ff91a56aa
Patrick Huck <phuck@lbl.gov>
2014-01-30 00:39:47 -0500

    utils: zip_flat extended to take two args

--- utils.py
+++ utils.py
@@ -5,8 +5,7 @@ def getOpts(i):
   """convience function for easy access to gnuplot property string"""
   return 'lt 1 lw 4 ps 2 lc %s pt 18' % default_colors[i]
 
-def zip_flat(a, b, c):
-  """zips three lists and flattens the result"""
-  return list(
-    itertools.chain.from_iterable(zip(a, b, c))
-  )
+def zip_flat(a, b, c=None):
+  """zips two or three lists and flattens the result"""
+  zipped = zip(a, b) if c is None else zip(a, b, c)
+  return list(itertools.chain.from_iterable(zipped))
